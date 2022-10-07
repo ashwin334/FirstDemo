f09f61c047d7561fc825af5655f194935d63c098
ashwin333 <ashwinpatil832@gmail.com>
2022-10-07 21:16:08 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int year = 2022;
+            string month = "Sept";
+            int day = 29;
+            
+            // interpolation --> $ sign
+
+            Console.WriteLine($"/n Day is {day} Current year is {year} /t & month is {month}");
+
+        }
+    }
+}
